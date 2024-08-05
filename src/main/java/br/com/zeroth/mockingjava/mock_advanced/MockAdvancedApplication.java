3d659aa3437651dde118d00b42401df2bd771e53
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2024-08-04 22:47:54 -0300

    setup

---
+++ src/main/java/br/com/zeroth/mockingjava/mock_advanced/MockAdvancedApplication.java
@@ -0,0 +1,13 @@
+package br.com.zeroth.mockingjava.mock_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zeroth/mockingjava/mock_advanced/MockAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zeroth.mockingjava.mock_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
